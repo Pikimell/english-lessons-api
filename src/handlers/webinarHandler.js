5874fa1b05b1f6122fae2f93824e0710d650b68b
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2024-11-10 13:50:22 +0200

    Add handlers for Webinar CRUD operations

---
+++ src/handlers/webinarHandler.js
@@ -0,0 +1,27 @@
+import { ctrlWrapper } from '../../utils/ctrlWrapper.js';
+import {
+  createWebinarController,
+  getWebinarController,
+  updateWebinarController,
+  deleteWebinarController,
+} from '../../controllers/webinarController.js';
+
+export const createWebinarHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(createWebinarController);
+  return await ctrl(event, context);
+};
+
+export const getWebinarHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(getWebinarController);
+  return await ctrl(event, context);
+};
+
+export const updateWebinarHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(updateWebinarController);
+  return await ctrl(event, context);
+};
+
+export const deleteWebinarHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(deleteWebinarController);
+  return await ctrl(event, context);
+};
