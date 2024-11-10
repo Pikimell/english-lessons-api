ff4dbbc76fb5d0000dc29225c999a6d5c7dd78f5
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2024-11-10 13:50:22 +0200

    Add handlers for Schedule CRUD operations

---
+++ src/handlers/scheduleHandler.js
@@ -0,0 +1,27 @@
+import { ctrlWrapper } from '../../utils/ctrlWrapper.js';
+import {
+  createScheduleController,
+  getScheduleController,
+  updateScheduleController,
+  deleteScheduleController,
+} from '../../controllers/scheduleController.js';
+
+export const createScheduleHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(createScheduleController);
+  return await ctrl(event, context);
+};
+
+export const getScheduleHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(getScheduleController);
+  return await ctrl(event, context);
+};
+
+export const updateScheduleHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(updateScheduleController);
+  return await ctrl(event, context);
+};
+
+export const deleteScheduleHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(deleteScheduleController);
+  return await ctrl(event, context);
+};
