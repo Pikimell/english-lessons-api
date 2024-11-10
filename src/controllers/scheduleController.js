ac1fa6251c51de95a292a19645a9be43ea82f40e
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2024-11-10 13:46:51 +0200

    Add CRUD operations for Schedule controller

---
+++ src/controllers/scheduleController.js
@@ -0,0 +1,32 @@
+import { response } from '../helpers/response.js';
+import {
+  createSchedule,
+  getScheduleById,
+  updateSchedule,
+  deleteSchedule,
+} from '../services/scheduleService.js';
+
+export const createScheduleController = async (event, context) => {
+  const scheduleData = event.body;
+  const schedule = await createSchedule(scheduleData);
+  return response(201)(schedule);
+};
+
+export const getScheduleController = async (event, context) => {
+  const { id } = event.pathParameters;
+  const schedule = await getScheduleById(id);
+  return response(200)(schedule);
+};
+
+export const updateScheduleController = async (event, context) => {
+  const { id } = event.pathParameters;
+  const updateData = event.body;
+  const schedule = await updateSchedule(id, updateData);
+  return response(200)(schedule);
+};
+
+export const deleteScheduleController = async (event, context) => {
+  const { id } = event.pathParameters;
+  const schedule = await deleteSchedule(id);
+  return response(200)(schedule);
+};
