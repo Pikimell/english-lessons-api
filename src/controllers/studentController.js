fa2daf768e5e68f719dc8b3236761a1e1b159d9e
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2024-11-10 13:46:51 +0200

    Add CRUD operations for Student controller

---
+++ src/controllers/studentController.js
@@ -0,0 +1,32 @@
+import { response } from '../helpers/response.js';
+import {
+  createStudent,
+  getStudentById,
+  updateStudent,
+  deleteStudent,
+} from '../services/studentService.js';
+
+export const createStudentController = async (event, context) => {
+  const studentData = event.body;
+  const student = await createStudent(studentData);
+  return response(201)(student);
+};
+
+export const getStudentController = async (event, context) => {
+  const { id } = event.pathParameters;
+  const student = await getStudentById(id);
+  return response(200)(student);
+};
+
+export const updateStudentController = async (event, context) => {
+  const { id } = event.pathParameters;
+  const updateData = event.body;
+  const student = await updateStudent(id, updateData);
+  return response(200)(student);
+};
+
+export const deleteStudentController = async (event, context) => {
+  const { id } = event.pathParameters;
+  const student = await deleteStudent(id);
+  return response(200)(student);
+};
