46c21262391be0559d7b0797c35ca7990202a604
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2024-11-10 13:50:22 +0200

    Add handlers for Test CRUD operations

---
+++ src/handlers/testHandler.js
@@ -0,0 +1,27 @@
+import { ctrlWrapper } from '../../utils/ctrlWrapper.js';
+import {
+  createTestController,
+  getTestController,
+  updateTestController,
+  deleteTestController,
+} from '../../controllers/testController.js';
+
+export const createTestHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(createTestController);
+  return await ctrl(event, context);
+};
+
+export const getTestHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(getTestController);
+  return await ctrl(event, context);
+};
+
+export const updateTestHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(updateTestController);
+  return await ctrl(event, context);
+};
+
+export const deleteTestHandler = async (event, context) => {
+  const ctrl = ctrlWrapper(deleteTestController);
+  return await ctrl(event, context);
+};
